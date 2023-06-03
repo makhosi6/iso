b92a16f3e281c555ca40942d4a91b871872b2a26
makhosi6 <ndondo330@gmail.com>
2023-06-03 16:52:20 +0200

    Sat  3 Jun 16:52:20 SAST 2023 - last commit from Zen

--- memory.js
+++ memory.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
@@ -30,36 +29,3 @@ server.listen(3004, () => {
 })
 
 
-=======
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-// Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
-
-// Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query)
-})
-
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
-server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now()
-  }
-  // Continue to JSON Server router
-  next()
-})
-
-// Use default router
-server.use(router)
-server.listen(3004, () => {
-  console.log('JSON Server is running')
-})
-
-
->>>>>>> d9072ce (Updates)
